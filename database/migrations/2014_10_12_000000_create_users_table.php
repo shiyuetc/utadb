5c76dd8fca60abf537507f112e74c6ca0101b014
shiyuetc <shiyuetc@users.noreply.github.com>
2019-05-21 18:17:12 +0900

    Add migration file

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,10 +14,16 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
+            $table->increments('id')->comment('主キー');
+            $table->string('screen_name', 15)->unique()->comment('ユーザー識別用ID');
+            $table->string('name', 20)->comment('ユーザー名');
+            $table->string('description', 255)->nullable()->comment('紹介文');
+            $table->integer('stacked_state_count')->default(0)->comment('気になる曲の数');
+            $table->integer('training_state_count')->default(0)->comment('練習中の曲の数');
+            $table->integer('mastered_state_count')->default(0)->comment('歌える曲の数');
+            $table->text('profile_image_url')->nullable()->comment('プロフィール画像のURL');
+            $table->string('email')->unique()->nullable()->comment('メールアドレス');
+            $table->string('password')->comment('ログインパスワード');
             $table->rememberToken();
             $table->timestamps();
         });
