6eba0e630f4332e10cdf0b1b38b29cfcc435bdd0
shiyuetc <shiyuetc@users.noreply.github.com>
2019-05-21 18:19:22 +0900

    Add seed file

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            [ 'screen_name' => 'test1', 'name' => 'test_user1', 'password' => bcrypt('test1'), 'remember_token' => str_random(10) ],
+            [ 'screen_name' => 'test2', 'name' => 'test_user2', 'password' => bcrypt('test2'), 'remember_token' => str_random(10) ],
+            [ 'screen_name' => 'test3', 'name' => 'test_user3', 'password' => bcrypt('test3'), 'remember_token' => str_random(10) ],
+        ]);
+    }
+}
