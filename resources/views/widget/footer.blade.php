81209ea94fe5cb954334414e35dd8b888f434ce4
shiyuetc <shiyuetc@users.noreply.github.com>
2019-05-24 00:01:57 +0900

    Add footer

---
+++ resources/views/widget/footer.blade.php
@@ -0,0 +1,12 @@
+<div class="footer">
+  <div class="footer-wrapper">
+    <div class="external">
+      <a href="https://twitter.com/sakuflip" target="_blank" rel="noopener"><i class="fab fa-twitter"></i>&nbsp;Twitter</a>
+      <a href="https://github.com/shiyuetc/utadb" target="_blank" rel="noopener"><i class="fab fa-github"></i>&nbsp;Github</a>
+    </div>
+    <div class="copyright">
+      <p>Copyright &copy; 2018/05 ~ 2019 shiyu</p>
+    </div>
+  </div>
+</div>
+<script src="{{ mix('js/app.js') }}"></script>
