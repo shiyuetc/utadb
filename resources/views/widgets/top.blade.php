00e7e0aef61497927a593fafa733cf833e948dbd
shiyuetc <shiyuetc@users.noreply.github.com>
2019-05-25 19:55:06 +0900

    Add prompt element

--- resources/views/widgets/top.blade.php
+++ resources/views/widgets/top.blade.php
@@ -4,8 +4,11 @@
     <h1 class="title">Utad<span style="opacity: 0.6;">ata</span>b<span style="opacity: 0.6;">ase</span></h1>
     <p class="text">Utadb は自分の持ち歌（歌える曲）や気になった曲を記録して管理したりユーザー同士で共有ができるWebサービスです。<br>カラオケ等へ行くとき用にメモ感覚で簡単に使えます。</p>
   </div>
+  <div class="prompt">
+    <button class="button button-danger auto" onclick="location.href='{{ route('register') }}'">今すぐ曲の管理を始める</button>
+  </div>
   <div class="introducts inner-margin">
-    <div class="introduct animated fadeInUp" style="animation-delay: 0.5s;">
+    <div class="introduct animated fadeIn" style="animation-delay: 0.2s;">
       <div class="description">
         <h2 class="title">分かりやすい持ち曲の管理</h2>
         <p class="text">曲の状態をメモ記録、習得中、習得済みの3つに振り分けて現在の状態を一目できます。</p>
@@ -18,15 +21,28 @@
         <img src="images/captures/training.jpg" alt="capture">
       </div>
     </div>
-    <div class="introduct animated fadeInUp" style="animation-delay: 1.5s;">
+    <div class="introduct animated fadeIn" style="animation-delay: 1.2s;">
       <div class="description">
         <h2 class="title">シンプルなタイムライン機能</h2>
         <p class="text">自分もしくは他のユーザーの更新をリスト形式で表示され、このページからサンプル音源の視聴や状態の更新を行うことが出来ます。</p>
-        <p class="text text-small">※曲はiTunes及びDAMで配信されている項目を取得できます。
+        <p class="text text-small">※曲はiTunes及びDAMで配信されている項目を取得できます。</p>
       </div>
       <div class="capture">
         <img src="images/captures/timeline.jpg" alt="capture">
       </div>
     </div>
+    <div class="introduct animated fadeIn" style="animation-delay: 2.2s;">
+      <div class="description">
+        <h2 class="title">他のユーザーとの共有</h2>
+        <p class="text">他のユーザーがそれぞれの状態に登録している曲やお互いに習得済みに登録している曲のリストを確認できます。</p>
+        <p class="text text-small">※この項目に関しては今後のアップデートで徐々に追加していく予定です。</p>
+      </div>
+      <div class="capture">
+        <img src="images/captures/users.jpg" alt="capture">
+      </div>
+    </div>
+  </div>
+  <div class="prompt">
+    <button class="button button-danger auto" onclick="location.href='{{ route('register') }}'">今すぐ曲の管理を始める</button>
   </div>
 </div>
