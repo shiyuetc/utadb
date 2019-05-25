ad679f8be809ed6b02739828e6d14947257a4fbe
shiyuetc <shiyuetc@users.noreply.github.com>
2019-05-25 15:13:22 +0900

    Add welcome page

---
+++ resources/views/components/top.blade.php
@@ -0,0 +1,32 @@
+<div class="top">
+  <div class="banner inner-padding">
+    <img id="icon" src="images/icons/icon-48x.png" alt="icon">
+    <h1 class="title">Utad<span style="opacity: 0.6;">ata</span>b<span style="opacity: 0.6;">ase</span></h1>
+    <p class="text">Utadb は自分の持ち歌（歌える曲）や気になった曲を記録して管理したりユーザー同士で共有ができるWebサービスです。<br>カラオケ等へ行くとき用にメモ感覚で簡単に使えます。</p>
+  </div>
+  <div class="introducts inner-margin">
+    <div class="introduct animated fadeInUp" style="animation-delay: 0.5s;">
+      <div class="description">
+        <h2 class="title">分かりやすい持ち曲の管理</h2>
+        <p class="text">曲の状態をメモ記録、習得中、習得済みの3つに振り分けて現在の状態を一目できます。</p>
+        <p class="text text-small">※分け方の目安<br>
+          メモ記録 : 気になっている又は、習得する予定の曲等<br>
+          習得中　 : 練習中や、歌詞を覚えている途中の曲等<br>
+          習得済み : 既に習得していて最後まで歌える状態の曲等</p>
+      </div>
+      <div class="capture">
+        <img src="images/captures/training.jpg" alt="capture">
+      </div>
+    </div>
+    <div class="introduct animated fadeInUp" style="animation-delay: 1.5s;">
+      <div class="description">
+        <h2 class="title">シンプルなタイムライン機能</h2>
+        <p class="text">自分もしくは他のユーザーの更新をリスト形式で表示され、このページからサンプル音源の視聴や状態の更新を行うことが出来ます。</p>
+        <p class="text text-small">※曲はiTunes及びDAMで配信されている項目を取得できます。
+      </div>
+      <div class="capture">
+        <img src="images/captures/timeline.jpg" alt="capture">
+      </div>
+    </div>
+  </div>
+</div>
--- resources/views/pages/welcome.blade.php
+++ resources/views/pages/welcome.blade.php
@@ -1,4 +1,5 @@
 @extends('layouts.app')
-@section('content')
 
+@section('content')
+@include('components.top')
 @endsection
