aaf64690a8a8b2f881b22b3d057b1514c091e966
shiyuetc <shiyuetc@users.noreply.github.com>
2019-05-26 10:24:17 +0900

    Change favicon

--- resources/views/const/head.blade.php
+++ resources/views/const/head.blade.php
@@ -12,6 +12,7 @@
 <meta property="og:site_name" content="utadb">
 <meta property="og:locale" content="ja_JP">
 <title>{{ config('app.name', 'Utadb') }}</title>
+<link rel="icon" type="image/png" href="{{ asset('favicon.ico') }}">
 <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 <link rel="stylesheet" type="text/css" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.0/animate.min.css">
