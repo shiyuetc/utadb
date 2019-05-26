14a5caa84a7685438455e02aab9d4c6d63f95cfa
shiyuetc <shiyuetc@users.noreply.github.com>
2019-05-26 10:25:06 +0900

    Add layout options

--- resources/views/const/main.blade.php
+++ resources/views/const/main.blade.php
@@ -1,7 +1,9 @@
 <div class="header-push"></div>
+@if(!isset($head_margin) || $head_margin)<div style="height: 12px;"></div>@endif
 <div class="main">
+  @if(!isset($visible_alert) || $visible_alert)@include('components.alert')@endif
   @yield('content')
-  @include('widgets.page-top')
 </div>
-<div class="height-small"></div>
+@include('widgets.page-top')
+<div class="height-large"></div>
 <div class="footer-push"></div>
--- resources/views/pages/welcome.blade.php
+++ resources/views/pages/welcome.blade.php
@@ -1,4 +1,8 @@
 @extends('layouts.app')
+@php
+$head_margin = false;
+$alert_visible = false;
+@endphp
 
 @section('content')
 @include('widgets.top')
