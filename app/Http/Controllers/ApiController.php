f0a9b2cd490f9f72e055722fd89590cebcc60100
shiyuetc <shiyuetc@users.noreply.github.com>
2019-05-28 22:00:20 +0900

    Fix api of get public timeline

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -3,13 +3,23 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use DB;
 use App\Models\Status;
 
 class ApiController extends Controller
 {
     public function publicTimeline()
     {
-        $statuses = Status::with(['user', 'song'])->get();
+        $statuses = Status::select('user_statuses.id', 'user_statuses.user_id', 'user_statuses.song_id', 'user_statuses.state', DB::raw('IFNULL(s1.state, 0) as user_state'), 'user_statuses.used_at')
+        ->leftjoin('user_statuses as s1', function($join) {
+            $join->where('s1.user_id', Auth::id())
+            ->on('user_statuses.song_id', '=', 's1.song_id');
+        })
+        ->with(['user', 'song'])
+        ->orderBy('used_at', 'desc')
+        ->get();
+            
         return response()->json($statuses);
     }
 }
