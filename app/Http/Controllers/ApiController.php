a90ec722fd80be6b299a5fa41c111965ab7dc92b
shiyuetc <shiyuetc@users.noreply.github.com>
2019-06-01 21:10:25 +0900

    Move get public timeline function to model

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -4,22 +4,16 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use DB;
+use App\Http\Requests\StatusRequest;
 use App\Models\Status;
+use App\Libraries\SongPuller\Puller;
+use Exception;
 
 class ApiController extends Controller
 {
     public function publicTimeline()
     {
-        $statuses = Status::select('user_statuses.id', 'user_statuses.user_id', 'user_statuses.song_id', 'user_statuses.state', DB::raw('IFNULL(s1.state, 0) as user_state'), 'user_statuses.used_at')
-        ->leftjoin('user_statuses as s1', function($join) {
-            $join->where('s1.user_id', Auth::id())
-            ->on('user_statuses.song_id', '=', 's1.song_id');
-        })
-        ->with(['user', 'song'])
-        ->orderBy('used_at', 'desc')
-        ->get();
-            
+        $statuses = Status::publicTimeline();
         return response()->json($statuses);
     }
 }
--- app/Models/Status.php
+++ app/Models/Status.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use DB;
 
 class Status extends Model
 {
@@ -28,4 +29,15 @@ class Status extends Model
         return $this->belongsTo('App\Models\Song');
     }
     
+    public static function publicTimeline()
+    {
+        return Status::select('user_statuses.id', 'user_statuses.user_id', 'user_statuses.song_id', 'user_statuses.state', DB::raw('IFNULL(s1.state, 0) as user_state'), 'user_statuses.used_at')
+        ->leftjoin('user_statuses as s1', function($join) {
+            $join->where('s1.user_id', auth()->id())
+            ->on('user_statuses.song_id', '=', 's1.song_id');
+        })
+        ->with(['user', 'song'])
+        ->orderBy('used_at', 'desc')
+        ->get();
+    }
 }
