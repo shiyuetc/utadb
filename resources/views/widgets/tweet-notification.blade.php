3137a42f4cee378560b46b984aa82399885fc102
shiyuetc <shiyuetc@users.noreply.github.com>
2019-06-02 22:20:51 +0900

    Fix art

--- resources/views/widgets/tweet-notification.blade.php
+++ resources/views/widgets/tweet-notification.blade.php
@@ -1,4 +1,4 @@
 @section('js_twitter_widgets', true)
-<div class="section section-hide animated fadeIn" style="animation-delay: 0.2s">
+<div class="section section-hide animated fadeIn" style="min-height:800px; animation-delay: 0.3s">
   <a class="twitter-timeline" data-lang="ja" data-tweet-limit="4" href="https://twitter.com/sakuflip"></a>
 </div>
