246f56ce5e4c10323d297b65526a513f224d5804
shiyuetc <shiyuetc@users.noreply.github.com>
2019-06-05 12:11:34 +0900

    Tweak

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -3,9 +3,8 @@
 @section('sidebar')
 @include('widgets.user-infomation', ['user' => Auth::user(), 'hide' => true])
 @include('widgets.tweet-notification')
-
 @endsection
+
 @section('content')
 <timeline-component/>
-
 @endsection
--- resources/views/pages/user-status.blade.php
+++ resources/views/pages/user-status.blade.php
@@ -3,8 +3,8 @@
 
 @section('sidebar')
 @include('widgets.user-infomation', ['user' => $user])
-
 @endsection
+
 @section('content')
 <user-statuses-component :state="'{{ $state['index'] }}'" :user_id="'{{ $user->id }}'"/>
 @endsection
