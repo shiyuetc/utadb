e93495b0190fb2e36584203bbeec1b4d570d4baa
shiyuetc <shiyuetc@users.noreply.github.com>
2019-06-05 11:49:36 +0900

    Fix prompt component

--- resources/views/pages/welcome.blade.php
+++ resources/views/pages/welcome.blade.php
@@ -3,7 +3,9 @@
 
 @section('content')
 @include('widgets.banner')
-@include('components.ui.prompt')
+<div class="prompt">
+  <button class="button button-danger auto" onclick="location.href='{{ route('register') }}'">今すぐ曲の管理を始める</button>
+</div>
 <div class="introducts margin-const">
   @component('components.introduct', ['delay_time' => '0.2', 'capture_image' => 'training.jpg'])
     @slot('title')
@@ -43,5 +45,4 @@
   @endcomponent
 </div>
 <div class="height-small"></div>
-@include('components.ui.prompt')
 @endsection
