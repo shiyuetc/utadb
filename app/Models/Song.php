a603c36ee0ab8f27d8f80706ab57b7f139287fd1
shiyuetc <shiyuetc@users.noreply.github.com>
2019-06-06 11:25:46 +0900

    add create song method

--- app/Models/Song.php
+++ app/Models/Song.php
@@ -17,4 +17,15 @@ class Song extends Model
     protected $hidden = [
         'artist_id', 'created_at'
     ];
+
+    public static function CreateSong($id, $title, $artist, $image_url = null, $audio_url = null)
+    {
+        $song = new Song();
+        $song->id = $id;
+        $song->title = $title;
+        $song->artist = $artist;
+        $song->image_url = $image_url;
+        $song->audio_url = $audio_url;
+        return $song;
+    }
 }
