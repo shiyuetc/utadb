8c505c20a2ce497b2e2e063cbbeb24dbd4bd0b7f
shiyuetc <shiyuetc@users.noreply.github.com>
2019-06-06 20:21:37 +0900

    Move update user statuses method from script

--- public/js/script.js
+++ public/js/script.js
@@ -5,3 +5,11 @@ function closeAlert() {
 		});
 	}
 }
+
+function updateUserStatuses(user) {
+	var statusCountElements = document.getElementsByClassName('status-count');
+  statusCountElements[0].textContent = (user.stacked_state_count + user.training_state_count + user.mastered_state_count) + '曲';
+  statusCountElements[1].textContent = user.mastered_state_count + '曲';
+  statusCountElements[2].textContent = user.training_state_count + '曲';
+  statusCountElements[3].textContent = user.stacked_state_count + '曲';
+}
--- resources/assets/js/components/timeline.vue
+++ resources/assets/js/components/timeline.vue
@@ -108,11 +108,7 @@ export default {
         }).then(res => {
           var user = res.data.user;
           if(this.user_id == null || this.user_id == user.id) {
-            var statusCountElements = document.getElementsByClassName('status-count');
-            statusCountElements[0].textContent = (user.stacked_state_count + user.training_state_count + user.mastered_state_count) + '曲';
-            statusCountElements[1].textContent = user.mastered_state_count + '曲';
-            statusCountElements[2].textContent = user.training_state_count + '曲';
-            statusCountElements[3].textContent = user.stacked_state_count + '曲';
+            updateUserStatuses(user);
           }
           this.isBusy = false;
         }).catch(err => {
