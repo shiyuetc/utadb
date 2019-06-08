0a4277ef39966885e8ac9d07255c28527858c8b6
shiyuetc <shiyuetc@users.noreply.github.com>
2019-06-09 00:45:44 +0900

    Add update select component

---
+++ resources/assets/js/components/update-select.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <select class="status-select" v-bind:class="[this.id , { 'active' : stateValue != 0 }]"
+      v-model="stateValue"
+      v-on:change="updateStatus()"
+      v-bind:disabled="this.$parent.isBusy">
+      <option value="0" selected>記録なし</option>
+      <option value="1">気になる</option>
+      <option value="2">練習中</option>
+      <option value="3">習得済み</option>
+    </select>
+  </div>
+</template>
+<script>
+export default {
+  props: ['id', 'state'],
+  data() {
+    return {
+      stateValue: this.state,
+    }
+  },
+  methods: {
+    updateStatus: function() {
+      if (this.$parent.isBusy) return;
+      this.$parent.isBusy = true;
+      axios.post("/api/update_status", {
+          song_id: this.id,
+          state: this.stateValue
+        }).then(res => {
+          this.$emit('updated', res.data);
+          this.$parent.isBusy = false;
+        }).catch(err => {
+          window.location.href = "/login";
+      });
+    }
+  }
+}
+</script>
