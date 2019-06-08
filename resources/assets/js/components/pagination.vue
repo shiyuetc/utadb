c21e2adf2f6357955557dfb3611aa85d136ab1f4
shiyuetc <shiyuetc@users.noreply.github.com>
2019-06-08 21:46:33 +0900

    Add pagination component

---
+++ resources/assets/js/components/pagination.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="pagination">
+    <button class="button button-danger auto" @click="paging(-1)" v-bind:disabled="this.$parent.pageValue == 1">前のページ</button>
+    <a>{{ this.$parent.pageValue }}&nbsp;ページ</a>
+    <button class="button button-danger auto" @click="paging(1)" v-bind:disabled="this.$parent.pageValue == 9999 || this.$parent.statuses.length == 0">次のページ</button>
+  </div>
+</template>
+<script>
+export default {
+  methods: {
+    paging: function(direction) {
+      this.$parent.pageValue += direction;
+      this.$emit('paging');
+    }
+  }
+}
+</script>
