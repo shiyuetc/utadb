e5e210105d0b8339156265138abf5e8a45a15d8c
shiyuetc <shiyuetc@users.noreply.github.com>
2019-06-08 21:53:56 +0900

    Add load progress component

---
+++ resources/assets/js/components/load-progress.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="load-progress">
+    <div v-if="!this.$parent.isMounted" id="loading" class="load">
+      <p><img src="/images/loading.gif" alt="読み込み中..."></p>
+    </div>
+    <div v-if="this.$parent.isMounted && this.$parent.statuses.length == 0" id="loaded" class="load">
+      <p>項目が存在しませんでした。</p>
+    </div>
+  </div>
+</template>
