e31dfc8c9c2aced64382be18d59a098f3128f2e8
shiyuetc <shiyuetc@users.noreply.github.com>
2019-06-08 21:59:25 +0900

    Move load progress component style

--- resources/assets/js/components/load-progress.vue
+++ resources/assets/js/components/load-progress.vue
@@ -8,3 +8,22 @@
     </div>
   </div>
 </template>
+<style scoped>
+div.load {
+  display: table;
+  width: 100%;
+  height: 67px;
+  text-align: center;
+}
+div.load p {
+  display: table-cell;
+  font-size: 12px;
+  line-height: 67px;
+  vertical-align: middle;
+}
+div.load p img {
+  display: block;
+	margin: 0 auto;
+	padding: 0;
+}
+</style>
