25b1309348c81b243c6abd8d74e4568d1699a6e7
shiyuetc <shiyuetc@users.noreply.github.com>
2019-06-09 01:24:40 +0900

    Add search song form

--- resources/views/pages/search-song.blade.php
+++ resources/views/pages/search-song.blade.php
@@ -1,10 +1,31 @@
 @extends('layouts.app')
-@section('title', "検索")
+@section('title', "曲の検索")
 
 @section('sidebar')
 @include('widgets.user-infomation', ['user' => Auth::user(), 'hide' => true])
 
 @endsection
 @section('content')
-<search-song-component/>
+<div class="section">
+  <h1 class="title"><i class="fas fa-music"></i>&nbsp;曲の検索</h1>
+  <form class="search" method="GET" action="#">
+    <table class="form-table">
+      <tr>
+        <td>
+          <select class="search-select" name="source">
+            <option value="0">iTunesから検索</option>
+            <option value="1" {{ Request::input('source') == 1 ? ' selected' : '' }}>DAMから検索</option>
+          </select>
+        </td>
+        <td width="100%">
+          <input class="text" value="{{ Request::input('q') }}" name="q" placeholder="検索キーワード" maxlength="20" autocomplete="off" required>
+        </td>
+        <td nowrap>
+          <button class="button button-info const-height"><i class="fa fa-search"></i><span class="hidden-md-below">&nbsp;検索</span></button>
+        </td>
+      </tr>
+    </table>
+  </form>
+</div>
+<!--<search-song-component/>-->
 @endsection
