513e9cf498a24488c07a427ecaf4654243df835a
shiyuetc <shiyuetc@users.noreply.github.com>
2019-06-09 15:30:18 +0900

    Fix api validator

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -2,20 +2,34 @@
 
 namespace App\Http\Controllers;
 
+use Validator;
 use Illuminate\Http\Request;
+use Illuminate\Http\Exceptions\HttpResponseException;
 use Illuminate\Support\Facades\Auth;
-use App\Http\Requests\StatusRequest;
+use App\Http\Requests\ApiRequestRules;
 use App\Models\Status;
 
 class ApiController extends Controller
 {   
+    private function QueryValidate($request, $validate = null)
+    {
+        $validator = Validator::make($request->all(), $validate);
+        if($validator->fails()) {
+            $response['errors']  = current(array_slice($validator->errors()->toArray(), 0, 1, true))[0];
+            throw new HttpResponseException(response()->json($response, 422));
+        }
+    }
+
     public function showStatus(Request $request)
     {
+        $this->QueryValidate($request, [
+            'song_id' => ApiRequestRules::getSongIdRule(),
+        ]);
         $status = Status::showStatus($request->song_id);
         return response()->json($status);
     }
 
-    public function updateStatus(StatusRequest $request)
+    public function updateStatus(Request $request)
     {
         $status = Status::updateStatus($request->song_id, $request->state);
         return response()->json($status);
---
+++ app/Http/Requests/ApiRequestRules.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\Validator;
+
+class ApiRequestRules
+{
+    public static function getPageRule()
+    {
+        return 'required|numeric|between:1,9999';
+    }
+
+    public static function getQRule()
+    {
+        return 'required|string|between:1,20';
+    }
+
+    public static function getSourceRule()
+    {
+        return 'required|numeric|between:0,1';
+    }
+
+    public static function getSongIdRule()
+    {
+        return 'required|string|digits_between:5,18|regex:/^[0-1].*/';
+    }
+
+    public static function getStateRule()
+    {
+        return 'required|numeric|between:0,3';
+    }
+
+    public static function getUserIdRule()
+    {
+        return 'required|numeric';
+    }
+}
