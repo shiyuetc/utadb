d6fcbe52e2b4f07b1e286ab5819bef39366dd829
shiyuetc <shiyuetc@users.noreply.github.com>
2019-06-09 20:00:40 +0900

    Add search user page

---
+++ resources/views/pages/search-user.blade.php
@@ -0,0 +1,30 @@
+@extends('layouts.app')
+@section('title', "ユーザーの検索")
+
+@section('sidebar')
+@include('widgets.user-infomation', ['user' => Auth::user(), 'hide' => true])
+@endsection
+
+@section('content')
+<div class="section">
+  <h1 class="title"><i class="fas fa-users"></i>&nbsp;ユーザーの検索</h1>
+  <form class="search" method="GET" action="{{ route('search.user') }}">
+    <table class="form-table">
+      <tr>
+        <td width="100%">
+          <input class="text" value="{{ Request::input('q') }}" name="q" placeholder="検索キーワード" maxlength="20" autocomplete="off" required>
+        </td>
+        <td nowrap>
+          <button class="button button-info const-height"><i class="fa fa-search"></i><span class="hidden-md-below">&nbsp;検索</span></button>
+        </td>
+      </tr>
+    </table>
+  </form>
+</div>
+@if(isset($q) && $q != '')
+<div class="section">
+  <h1 class="title"><i class="fa fa-search"></i>&nbsp;検索結果</h1>
+  <search-user-component :q="'{{ urlencode($q) }}'" :page="{{ $page }}"/>
+</div>
+@endif
+@endsection
