37890d1a45bd2f25628ffd252bbf6b1ca882d1c8
shiyuetc <shiyuetc@users.noreply.github.com>
2019-06-09 20:00:29 +0900

    Fix table style

--- resources/assets/js/components/search-song.vue
+++ resources/assets/js/components/search-song.vue
@@ -1,29 +1,34 @@
 <template>
   <div class="search-result">
     <loadProgress/>
-    <div v-if="this.isMounted" class="statuses">
-      <div class="status" v-for="(status, index) in statuses" :key='index'>
-        <table class="music-table">
-          <tr>
-            <td class="media-cell">
-              <div class="cover-image">
-                <img v-bind:src="status.song.image_url" alt="">
-                <div class="mediPlayer" v-if="status.song.audio_url">
-                  <audio class="listen" preload="none" data-size="40" v-bind:src="status.song.audio_url"></audio>
-                </div>
+    <table v-if="this.isMounted" class="object-table music-table table-padding">
+      <thead v-if="this.statuses.length != 0">
+        <tr>
+          <th></th>
+          <th class="title-column">タイトル</th>
+          <th>ステータス</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(status, index) in statuses" :key='index'>
+          <td class="media-cell">
+            <div class="cover-image">
+              <img v-bind:src="status.song.image_url" alt="">
+              <div class="mediPlayer" v-if="status.song.audio_url">
+                <audio class="listen" preload="none" data-size="40" v-bind:src="status.song.audio_url"></audio>
               </div>
-            </td>
-            <td class="text-cell">
-              <p class="title"><a class="default-link" v-bind:href="'/songs/' + status.song.id">{{ status.song.title }}</a></p>
-              <p class="artist">{{ status.song.artist }}</p>
-            </td>
-            <td class="action-cell">
-              <updateSelect @updated="updatedStatus" :id="status.song.id" :state="status.user_state"/>
-            </td>
-          </tr>
-        </table>
-      </div>
-    </div>
+            </div>
+          </td>
+          <td class="text-cell">
+            <p class="title"><a class="default-link" v-bind:href="'/songs/' + status.song.id">{{ status.song.title }}</a></p>
+            <p class="artist">{{ status.song.artist }}</p>
+          </td>
+          <td class="action-cell">
+            <updateSelect @updated="updatedStatus" :id="status.song.id" :state="status.user_state"/>
+          </td>
+        </tr>
+      </tbody>
+    </table>
     <pagination @paging="statusesRequest"/>
   </div>
 </template>
