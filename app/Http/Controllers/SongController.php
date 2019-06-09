268644a7d70fe216eea1358c9b6439b0f8294acd
shiyuetc <shiyuetc@users.noreply.github.com>
2019-06-09 15:30:37 +0900

    Fix parameter

--- app/Http/Controllers/SongController.php
+++ app/Http/Controllers/SongController.php
@@ -20,9 +20,10 @@ class SongController extends Controller
 
     public function search(Request $request)
     {
-        $source = isset($request->source) ? $request->source : 0;
+        $source = (isset($request->source) && $request->source >= 0 && $request->source <= 1) ? $request->source : 0;
         $q = isset($request->q) ? trim($request->q) : '';
         $page = (isset($request->page) && $request->page >= 1 && $request->page <= 9999) ? $request->page : 1;
+
         return view('pages.search-song', ['source' => $source, 'q' => $q, 'page' => $page]);
     }
 }
