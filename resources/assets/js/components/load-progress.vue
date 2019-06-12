91dc30e2ac68310c028ab86f36e6a45c662ffe2c
shiyuetc <shiyuetc@users.noreply.github.com>
2019-06-12 21:20:12 +0900

    Fix load progress component

--- resources/assets/js/components/load-progress.vue
+++ resources/assets/js/components/load-progress.vue
@@ -1,32 +1,46 @@
 <template>
   <div class="load-progress">
-    <div v-if="!this.$parent.isMounted && !this.$parent.isError" id="loading" class="load">
+    <div id="loading" v-if="!this.$parent.isMounted && !this.$parent.isError">
       <p><img src="/images/loading.gif" alt="読み込み中..."></p>
     </div>
-    <div v-if="this.$parent.isMounted && this.$parent.statuses.length == 0" id="loaded" class="load">
+    <div id="loaded" v-if="this.$parent.isMounted && this.responseCount == 0">
       <p>項目が見つかりませんでしたわ。(&gt; &lt;。)</p>
     </div>
-    <div v-if="this.$parent.isError" id="error" class="load">
+    <div id="error" v-if="this.$parent.isError">
       <p>エラー吐きましたわ。(&gt; &lt;。)</p>
     </div>
   </div>
 </template>
-<style scoped>
-div.load {
+<script>
+export default {
+  model: {
+    prop: 'responseCount'
+  },
+  props: {
+    responseCount: {
+      type: Number,
+      required: false,
+      default: -1
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+div.load-progress > div {
   display: table;
   width: 100%;
   height: 67px;
   text-align: center;
-}
-div.load p {
-  display: table-cell;
-  font-size: 12px;
-  line-height: 67px;
-  vertical-align: middle;
-}
-div.load p img {
-  display: block;
-	margin: 0 auto;
-	padding: 0;
+  p {
+    display: table-cell;
+    font-size: 12px;
+    line-height: 67px;
+    vertical-align: middle;
+    img {
+      display: block;
+      margin: 0 auto;
+      padding: 0;
+    }
+  }
 }
 </style>
