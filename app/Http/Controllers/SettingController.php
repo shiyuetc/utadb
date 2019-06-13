2b8adcb20b1116c1d0d9486d593ec96635ef728a
shiyuetc <shiyuetc@users.noreply.github.com>
2019-06-14 00:22:18 +0900

    Fix setting result mesasge

--- app/Http/Controllers/SettingController.php
+++ app/Http/Controllers/SettingController.php
@@ -2,9 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Deactivate;
 use App\Models\User;
 use App\Http\Requests\UpdateProfileRequest;
 use App\Http\Requests\UpdatePasswordRequest;
+use App\Http\Requests\UpdateDeactiveRequest;
 use Illuminate\Http\Request;
 
 class SettingController extends Controller
@@ -18,9 +20,9 @@ class SettingController extends Controller
     {
         $result = User::updateProfile($request->name, $request->description, $request->avatar);
         if($result) {
-            $alert = ['type' => 'success', 'text' => 'プロフィールの変更を保存しました。'];
+            $alert = ['type' => 'success', 'text' => 'プロフィールの変更を保存しました'];
         } else {
-            $alert = ['type' => 'danger', 'text' => 'プロフィールの変更に失敗しました。'];
+            $alert = ['type' => 'danger', 'text' => 'プロフィールの変更に失敗しました'];
         }
         
         return view('pages.settings.profile', ['alert' => $alert]);
@@ -35,9 +37,9 @@ class SettingController extends Controller
     {
         $result = User::updatePassword($request->password);
         if($result) {
-            $alert = ['type' => 'success', 'text' => 'パスワードの変更を保存しました。'];
+            $alert = ['type' => 'success', 'text' => 'パスワードの変更を保存しました'];
         } else {
-            $alert = ['type' => 'danger', 'text' => 'パスワードの変更に失敗しました。'];
+            $alert = ['type' => 'danger', 'text' => 'パスワードの変更に失敗しました'];
         }
         
         return view('pages.settings.password', ['alert' => $alert]);
