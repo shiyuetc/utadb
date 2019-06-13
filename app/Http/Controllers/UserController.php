2535b8617150cb8fba2ed5613225e61f1d0bd825
shiyuetc <shiyuetc@users.noreply.github.com>
2019-06-13 19:08:04 +0900

    Fix user controller

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,31 +2,23 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\User;
+use App\Models\Status;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    private $user = null;
-
-    public function __construct(Request $request)
+    public function __construct()
     {
-        $this->user = User::where('screen_name', $request->id)->first();
-    }
-    
-    public function index($id)
-    {
-        if(is_null($this->user)) {
-            return view('errors.404');
-        }
-        return view('pages.user', ['user' => $this->user]);
+        $this->middleware('user');
     }
 
-    public function status(Request $request, $id, $state)
+    public function index(Request $request)
     {
-        if(is_null($this->user)) {
-            return view('errors.404');
-        }
+        return view('pages.user', ['user' => $request->user]);
+    }
+
+    public function status(Request $request)
+    {
         $page = (isset($request->page) && $request->page >= 1 && $request->page <= 9999) ? $request->page : 1;
         $stateArray = [
             'all' => ['index' => 0, 'jp' => '登録済みの曲', 'en' => 'all', 'icon-class' => 'fa fa-check'], 
@@ -34,7 +26,22 @@ class UserController extends Controller
             'training' => ['index' => 2, 'jp' => '練習中の曲', 'en' => 'training', 'icon-class' => 'fas fa-graduation-cap'], 
             'mastered' => ['index' => 3, 'jp' => '習得済みの曲', 'en' => 'mastered', 'icon-class' => 'far fa-sticky-note'], 
         ];
-        return view('pages.user-status', ['user' => $this->user, 'state' => $stateArray[$state], 'page' => $page]);
+        return view('pages.user-status', ['user' => $request->user, 'state' => $stateArray[$request->state], 'page' => $page]);
+    }
+
+    public function random(Request $request)
+    {
+        $status = Status::select('song_id')
+            ->where('user_id', $request->user->id)
+            ->where('state', 3)
+            ->inRandomOrder()
+            ->first();
+        if(isset($status)) {
+            return redirect()->route('song', ['id' => $status['song_id']]);
+        } else {
+            $alert = ['type' => 'danger', 'text' => '習得済みに登録されている曲が見つかりませんでした'];
+            return view('pages.user', ['user' => $request->user, 'alert' => $alert]);
+        }
     }
 
     public function search(Request $request)
