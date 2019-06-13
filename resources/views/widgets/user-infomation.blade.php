51601de19177e14fa4e354ee6f1a5a49849e2f96
shiyuetc <shiyuetc@users.noreply.github.com>
2019-06-13 21:05:30 +0900

    Add event buttons to user infomation

--- resources/views/widgets/user-infomation.blade.php
+++ resources/views/widgets/user-infomation.blade.php
@@ -6,6 +6,12 @@
     <p class="name"><a class="default-link" href="{{ route('user', ['id' => $user->screen_name]) }}">{{ $user->name }}&nbsp;(&#64;{{ $user->screen_name }})</a></p>
     <p class="description">{{ $user->description }}</p>
     <p class="created"><i class="fa fa-calendar-alt"></i>&nbsp;{{ date('Y年m月d日',  strtotime($user->created_at)) }}に登録&nbsp;({{ (strtotime(date('Y-m-d')) - strtotime(date_format(date_create($user->created_at), 'Y-m-d'))) / 86400 }}日経過)</p>
+    @if($user->id == auth()->id())
+    <div class="event-buttons">
+      <button class="button button-default" onclick="location='{{ route('settings.profile') }}'">プロフィールの編集</button>
+      <button class="button button-default" onclick="location='{{ route('user.random', ['id' => $user->screen_name]) }}'">ランダム選曲</button>
+    </div>
+    @endif
   </div>
   <ul class="user-statuses list">
     <li class="{{ Request::is('@*/status/all') ? ' active' : '' }}"><a href="{{ route('user.status', ['id' => $user->screen_name, 'state' => 'all']) }}"><span class="hidden-sm-below"><i class="fa fa-check"></i>&nbsp;</span>登録済み<span class="hidden-md-below">の曲</span><span class="right-icon status-count">{{ $user->allStateCount() }}曲</span></a></li>
