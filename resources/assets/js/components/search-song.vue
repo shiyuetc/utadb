e671e5242b8f9d59d7c9ac31d557a3832808a7e5
shiyuetc <shiyuetc@users.noreply.github.com>
2019-06-17 09:39:29 +0900

    Fix search song components

--- resources/assets/js/components/search-song.vue
+++ resources/assets/js/components/search-song.vue
@@ -1,51 +1,25 @@
 <template>
   <div class="search-result">
     <loadProgress v-model="statuses.length"/>
-    <table v-if="this.isMounted" class="object-table music-table table-padding">
-      <thead v-if="this.statuses.length != 0">
-        <tr>
-          <th></th>
-          <th class="title-column">タイトル</th>
-          <th>ステータス</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="(status, index) in statuses" :key='index'>
-          <td class="media-cell">
-            <div class="cover-image">
-              <img v-bind:src="status.song.image_url" alt="">
-              <div class="mediPlayer" v-if="status.song.audio_url">
-                <audio class="listen" preload="none" data-size="40" v-bind:src="status.song.audio_url"></audio>
-              </div>
-            </div>
-          </td>
-          <td class="text-cell">
-            <p class="title"><a class="default-link" v-bind:href="'/songs/' + status.song.id">{{ status.song.title }}</a></p>
-            <p class="artist">{{ status.song.artist }}</p>
-          </td>
-          <td class="action-cell">
-            <updateSelect @updated="updatedStatus" :id="status.song.id" :state="status.user_state"/>
-          </td>
-        </tr>
-      </tbody>
-    </table>
+    <songs @updated="updatedStatus" v-model="this.statuses"/>
     <pagination @paging="statusesRequest" v-model="statuses.length"/>
   </div>
 </template>
 <script>
 import LoadProgress from './widgets/load-progress.vue';
 import Pagination from './widgets/pagination.vue';
-import UpdateSelect from './ui/update-select.vue';
+import Songs from './common/songs.vue';
 
 export default {
   components: {
     LoadProgress,
     Pagination,
-    UpdateSelect
+    Songs
   },
   props: {
     source: {
       type: Number,
+      required: false,
       default: 0
     },
     q: {
@@ -54,17 +28,17 @@ export default {
     },
     page: {
       type: Number,
+      required: false,
       default: 1
     }
   },
   data() {
     return {
+      isMounted: false,
+      isError: false,
       pageValue: this.page,
       statuses: [],
       setPlayer: null,
-      isMounted: false,
-      isBusy: false,
-      isError: false
     };
   },
   methods: {
