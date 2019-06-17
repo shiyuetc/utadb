dfb7f23fdcffc203e98d69eaaded937cb2b224b2
shiyuetc <shiyuetc@users.noreply.github.com>
2019-06-17 09:37:04 +0900

    Add songs components

---
+++ resources/assets/js/components/common/songs.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="songs">
+    <table v-if="this.$parent.isMounted && this.statuses.length != 0" class="object-table music-table table-padding">
+      <thead>
+        <tr>
+          <th></th>
+          <th class="title-column">タイトル</th>
+          <th>ステータス</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(status, index) in this.statuses" :key='index'>
+          <td class="media-cell">
+            <div class="cover-image">
+              <img v-bind:src="status.song.image_url" alt="">
+              <div class="mediPlayer" v-if="status.song.audio_url">
+                <audio class="listen" preload="none" data-size="40" v-bind:src="status.song.audio_url"></audio>
+              </div>
+            </div>
+          </td>
+          <td class="text-cell">
+            <p class="title"><a class="default-link" v-bind:href="'/songs/' + status.song.id">{{ status.song.title }}</a></p>
+            <p class="artist">{{ status.song.artist }}</p>
+          </td>
+          <td class="action-cell">
+            <updateSelect @updated="updatedStatus" :id="status.song.id" :state="status.user_state"/>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+<script>
+import UpdateSelect from '../ui/update-select.vue';
+
+export default {
+  components: {
+    UpdateSelect
+  },
+  model: {
+    prop: 'statuses'
+  },
+  props: {
+    statuses: {
+      type: Array,
+      required: true,
+    }
+  },
+  data() {
+    return {
+      isBusy: false,
+    };
+  },
+  methods: {
+    updatedStatus: function(response) {
+      this.$emit('updated', response);
+    }
+  } 
+}
+</script>
--- resources/assets/js/components/user-statuses.vue
+++ resources/assets/js/components/user-statuses.vue
@@ -1,47 +1,20 @@
 <template>
   <div class="user-statuses">
     <loadProgress v-model="this.statuses.length"/>
-    <table v-if="this.isMounted" class="object-table music-table table-padding">
-      <thead v-if="this.statuses.length != 0">
-        <tr>
-          <th></th>
-          <th class="title-column">タイトル</th>
-          <th>ステータス</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="(status, index) in this.statuses" :key='index'>
-          <td class="media-cell">
-            <div class="cover-image">
-              <img v-bind:src="status.song.image_url" alt="">
-              <div class="mediPlayer" v-if="status.song.audio_url">
-                <audio class="listen" preload="none" data-size="40" v-bind:src="status.song.audio_url"></audio>
-              </div>
-            </div>
-          </td>
-          <td class="text-cell">
-            <p class="title"><a class="default-link" v-bind:href="'/songs/' + status.song.id">{{ status.song.title }}</a></p>
-            <p class="artist">{{ status.song.artist }}</p>
-          </td>
-          <td class="action-cell">
-            <updateSelect @updated="updatedStatus" :id="status.song.id" :state="status.user_state"/>
-          </td>
-        </tr>
-      </tbody>
-    </table>
+    <songs @updated="updatedStatus" v-model="this.statuses"/>
     <pagination @paging="statusesRequest" v-model="this.statuses.length"/>
   </div>
 </template>
 <script>
 import LoadProgress from './widgets/load-progress.vue';
 import Pagination from './widgets/pagination.vue';
-import UpdateSelect from './ui/update-select.vue';
+import Songs from './common/songs.vue';
 
 export default {
   components: {
     LoadProgress,
     Pagination,
-    UpdateSelect
+    Songs
   },
   props: {
     user_id: {
@@ -62,7 +35,6 @@ export default {
   data() {
     return {
       isMounted: false,
-      isBusy: false,
       isError: false,
       pageValue: this.page,
       setPlayer: null,
