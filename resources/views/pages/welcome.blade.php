2395057e88d96e0c56f770febeced835562eea8d
shiyuetc <shiyuetc@users.noreply.github.com>
2019-06-18 06:00:46 +0900

    Move banner widget

--- resources/views/pages/welcome.blade.php
+++ resources/views/pages/welcome.blade.php
@@ -2,7 +2,11 @@
 @section('page_type', 'outer')
 
 @section('content')
-@include('widgets.banner')
+<div class="banner padding-const">
+  <img id="icon" class="animated rotateIn" src="{{ asset('images/icons/icon-48x.png') }}" alt="icon">
+  <h1 class="title">Utad<span style="opacity: 0.6;">ata</span>b<span style="opacity: 0.6;">ase</span></h1>
+  <p class="text">Utadb は自分の持ち歌（歌える曲）や気になった曲を記録して管理したりユーザー同士で共有ができるWebサービスです。<br>カラオケ等へ行くとき用にメモ感覚で簡単に使えます。</p>
+</div>
 <div class="prompt">
   <button class="button button-danger auto" onclick="location.href='{{ route('register') }}'">今すぐ曲の管理を始める</button>
 </div>
