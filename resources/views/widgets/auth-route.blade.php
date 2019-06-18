ec07cb246057872515f78d6bba0fe8410264c0c9
shiyuetc <shiyuetc@users.noreply.github.com>
2019-06-18 11:46:04 +0900

    Add auth route component

---
+++ resources/views/widgets/auth-route.blade.php
@@ -0,0 +1,7 @@
+<div class="section section-hide">
+  <div class="auth-route">
+    <button class="button button-danger" onclick="location='{{ route('register') }}'">新規登録</button>
+    <p>もしくは</p>
+    <button class="button button-danger" onclick="location='{{ route('login') }}'">ログイン</button>
+  </div>
+</div>
