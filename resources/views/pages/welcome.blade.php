0e7858f2fe466f652616c4fd488e247f933ec3e2
shiyuetc <shiyuetc@users.noreply.github.com>
2019-06-19 18:36:56 +0900

    Implement resource counter

--- resources/views/pages/welcome.blade.php
+++ resources/views/pages/welcome.blade.php
@@ -6,6 +6,7 @@
   <img id="icon" class="animated rotateIn" src="{{ asset('images/icons/icon-48x.png') }}" alt="icon">
   <h1 class="title">Utad<span style="opacity: 0.6;">ata</span>b<span style="opacity: 0.6;">ase</span></h1>
   <p class="text">Utadb は自分の持ち歌（歌える曲）や気になった曲を記録して管理したりユーザー同士で共有ができるWebサービスです。<br>カラオケ等へ行くとき用にメモ感覚で簡単に使えます。</p>
+  <resource-counter-component></resource-counter-component>
   <div class="prompt">
     <button class="button button-danger auto" onclick="location.href='{{ route('register') }}'">今すぐ曲の管理を始める</button>
   </div>
