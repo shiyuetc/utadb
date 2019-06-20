2f4b5fd9b971c541dd03466c0c77f8db7cc71437
shiyuetc <shiyuetc@users.noreply.github.com>
2019-06-21 00:06:58 +0900

    Implement section component of setting list widget

--- resources/views/widgets/setting-list.blade.php
+++ resources/views/widgets/setting-list.blade.php
@@ -1,6 +1,10 @@
-<div class="section">
-  <h1 class="title"><i class="fas fa-cog"></i>&nbsp;設定</h1>
-  <ul class="setting-list list">
-    <li class="{{ Request::is('settings/account') ? ' active' : '' }}"><a href="{{ route('settings.account') }}"><i class="fas fa-user"></i>&nbsp;アカウントの設定<span class="right-icon"><i class="fas fa-angle-double-right"></i></span></a></li>
-  </ul>
-</div>
+@component('components.section')
+  @slot('title')
+    <i class="fas fa-cog"></i>&nbsp;設定
+  @endslot
+  @slot('contents')
+    <ul class="setting-list list">
+      <li class="{{ Request::is('settings/account') ? ' active' : '' }}"><a href="{{ route('settings.account') }}"><i class="fas fa-user"></i>&nbsp;アカウントの設定<span class="right-icon"><i class="fas fa-angle-double-right"></i></span></a></li>
+    </ul>
+  @endslot
+@endcomponent
