5ed2d08ade6dd1756200436e0802523bb61ec4fb
shiyuetc <shiyuetc@users.noreply.github.com>
2019-06-20 23:59:52 +0900

    Add section component

---
+++ resources/views/components/section.blade.php
@@ -0,0 +1,8 @@
+<div class="section{{ isset($hidden) ? ' section-hidden' : '' }}{{ isset($padding) ? ' section-padding' : '' }}">
+  <div class="header">
+    <h1 class="title">{{ $title }}</h1>
+  </div>
+  <div class="contents">
+    {{ $contents }}
+  </div>
+</div>
