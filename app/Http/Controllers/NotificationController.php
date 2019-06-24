a7ea35a0d5abfa990ba009769c730818f7d20730
shiyuetc <shiyuetc@users.noreply.github.com>
2019-06-24 22:39:22 +0900

    通知ページで未認証の場合アクセス不可に

--- app/Http/Controllers/NotificationController.php
+++ app/Http/Controllers/NotificationController.php
@@ -6,6 +6,11 @@ use Illuminate\Http\Request;
 
 class NotificationController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     public function index() 
     {
         return view('pages.notification');
