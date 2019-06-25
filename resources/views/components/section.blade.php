1c19ad0b9212da5fe0e7e38a4c126b6def0bea54
shiyuetc <shiyuetc@users.noreply.github.com>
2019-06-26 01:03:37 +0900

    Fix section

--- resources/views/components/section.blade.php
+++ resources/views/components/section.blade.php
@@ -1,13 +1,13 @@
-<div class="section{{ isset($hidden) && $hidden ? ' section-hidden' : '' }}{{ isset($padding) && $padding ? ' section-padding' : '' }}">
+<div class="section{{ !empty($hidden) ? ' section-hidden' : '' }}{{ !empty($padding) ? ' section-padding' : '' }}">
   @isset($title)
-    <div class="header">
+<div class="header">
       <h1 class="title">{{ $title }}</h1>
-      @if(isset($toggle) && $toggle)
-        <span class="section-toggle{{ isset($toggleState) && $toggleState ? ' hidden' : '' }}" onclick="toggleSection(this)"><i class="fas fa-chevron-up"></i></span>
+      @if(!empty($toggle))
+        <span class="section-toggle{{ !empty($toggleState) ? ' hidden' : '' }}" onclick="toggleSection(this)"><i class="fas fa-chevron-up"></i></span>
       @endif
     </div>
   @endisset
-  <div class="contents" style="{{ isset($toggleState) && $toggleState ? 'display: none;' : '' }}">
+  <div class="contents" style="{{ !empty($toggleState) ? 'display: none;' : '' }}">
     {{ $contents }}
   </div>
 </div>
