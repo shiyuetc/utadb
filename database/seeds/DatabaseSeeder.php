9aec2c6ad072adf1b40d110f31cb2ad2ec9da930
shiyuetc <shiyuetc@users.noreply.github.com>
2019-07-01 08:09:08 +0900

    Add database seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -16,6 +16,7 @@ class DatabaseSeeder extends Seeder
             UsersTableSeeder::class,
             SongsTableSeeder::class,
             StatusesTableSeeder::class,
+            ActivitiesTableSeeder::class,
         ]);
     }
 }
