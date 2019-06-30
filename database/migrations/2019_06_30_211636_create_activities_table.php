e0714da027cc85c8b22716ee980fd534bac5c232
shiyuetc <shiyuetc@users.noreply.github.com>
2019-07-01 08:08:39 +0900

    Add create activity table migration

---
+++ database/migrations/2019_06_30_211636_create_activities_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateActivitiesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('activities', function (Blueprint $table) {
+            $table->string('id', 14)->comment('主キー');
+            $table->integer('user_id')->unsigned();
+            $table->string('song_id');
+            $table->integer('old_state')->comment('状態 [1:stacked, 2:training, 3:mastered]');
+            $table->integer('state')->comment('状態 [1:stacked, 2:training, 3:mastered]');
+            $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->primary('id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('song_id')->references('id')->on('songs');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('activities');
+    }
+}
