f45af13f4fc09949254c9272d9341c59438958d1
shiyuetc <shiyuetc@users.noreply.github.com>
2019-07-01 09:01:27 +0900

    Fix href link

--- resources/views/widgets/user-infomation.blade.php
+++ resources/views/widgets/user-infomation.blade.php
@@ -8,7 +8,7 @@
     <p class="created"><i class="fa fa-calendar-alt"></i>&nbsp;{{ date('Y年m月d日',  strtotime($user->created_at)) }}に登録&nbsp;({{ (strtotime(date('Y-m-d')) - strtotime(date_format(date_create($user->created_at), 'Y-m-d'))) / 86400 }}日経過)</p>
     <div class="event-buttons">
       @if($user->id == auth()->id())
-        <div><button class="button button-default" onclick="location='{{ route('settings.account') }}'">プロフィールの編集</button></div>
+        <div><button class="button button-default" onclick="location='{{ route('settings.profile') }}'">プロフィールの編集</button></div>
       @endif
       <div><button class="button button-default" onclick="location='{{ route('user.random', ['id' => $user->screen_name]) }}'">ランダム選曲</button></div>
     </div>
