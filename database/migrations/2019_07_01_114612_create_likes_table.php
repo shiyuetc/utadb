1968c5fdc287ff229a18bc586c3df5a48c719317
shiyuetc <shiyuetc@users.noreply.github.com>
2019-07-02 18:26:06 +0900

    Add create likes table migration

---
+++ database/migrations/2019_07_01_114612_create_likes_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLikesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('likes', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('activity_id', 14);
+            $table->integer('user_id')->unsigned();
+            $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->foreign('activity_id')->references('id')->on('activities');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->unique(['activity_id', 'user_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('likes');
+    }
+}
