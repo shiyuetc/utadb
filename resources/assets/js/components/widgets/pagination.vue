edc8776b759e5fdfc016658c140c04a3ac606ea9
shiyuetc <shiyuetc@users.noreply.github.com>
2019-07-05 09:42:45 +0900

    取得検索が20件未満の場合次ページボタンを無効に

--- resources/assets/js/components/widgets/pagination.vue
+++ resources/assets/js/components/widgets/pagination.vue
@@ -2,7 +2,7 @@
   <div class="pagination">
     <button class="button button-danger auto" @click="paging(-1)" v-bind:disabled="!this.$parent.isMounted || this.$parent.pageValue == 1">前のページ</button>
     <a>{{ this.$parent.pageValue }}&nbsp;ページ</a>
-    <button class="button button-danger auto" @click="paging(1)" v-bind:disabled="!this.$parent.isMounted || this.$parent.pageValue == 9999 || this.responseCount == 0">次のページ</button>
+    <button class="button button-danger auto" @click="paging(1)" v-bind:disabled="!this.$parent.isMounted || this.$parent.pageValue == 9999 || this.responseCount < 20">次のページ</button>
   </div>
 </template>
 <script>
