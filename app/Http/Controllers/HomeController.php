b27ce647050b3d9e6dddb62a0984f6e1b501d38d
shiyuetc <shiyuetc@users.noreply.github.com>
2019-07-05 21:18:57 +0900

    非利用時にアラートを表示するように

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -11,8 +11,12 @@ class HomeController extends Controller
     public function index()
     {
         if (Auth::check()) {
-            $exist_unconfirm_notification = Notification::existUnconfirm();
-            return view('pages.home', ['exist_unconfirm_notification' => $exist_unconfirm_notification]);
+            $response = [];
+            if(Auth::user()->allStateCount() == 0) {
+                $response['alert'] = ['type' => 'default', 'text' => 'まずは曲の検索からあなたの持ち歌を探しましょう'];
+            }
+            $response['exist_unconfirm_notification'] = Notification::existUnconfirm();
+            return view('pages.home', $response);
         } else {
             return view('pages.welcome');
         }
