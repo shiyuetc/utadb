7046a7ffaff5f2eaa9dbb79d0a37c800ab067896
shiyuetc <shiyuetc@users.noreply.github.com>
2019-07-06 20:43:30 +0900

    Add artist model

---
+++ app/Models/Artist.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+class Artist
+{
+    public $id;
+
+    public $name;
+    
+    public function __construct($id, $name)
+    {
+        $this->id = $id;
+        $this->name = $name;
+    }
+}
