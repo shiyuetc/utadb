e2f6831d6c307c8557c96ce2e4a0ea89b9c21795
shiyuetc <shiyuetc@users.noreply.github.com>
2019-07-06 16:13:06 +0900

    Move common count function

--- app/Http/Middleware/CheckUser.php
+++ app/Http/Middleware/CheckUser.php
@@ -3,6 +3,7 @@
 namespace App\Http\Middleware;
 
 use Closure;
+use App\Models\Status;
 use App\Models\User;
 
 class CheckUser
@@ -20,6 +21,7 @@ class CheckUser
         if(is_null($user)) {
             abort(404);
         }
+        $user->common_count = Status::commonCount($user->id);
         $request->merge(['user' => $user]);
         return $next($request);
     }
--- app/Models/Status.php
+++ app/Models/Status.php
@@ -17,10 +17,20 @@ class Status extends Model
 
     public $timestamps = false;
 
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
     protected $fillable = [
         'state', 'used_at'
     ];
 
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
     protected $hidden = [
         'user_id', 'song_id'
     ];
@@ -132,5 +142,19 @@ class Status extends Model
             DB::rollBack();
             throw new HttpResponseException(response()->json(['errors' => $e->getMessage()], 422));
         }
+    }
+    
+    public static function commonCount($user_id)
+    {
+        if(!auth()->check() || auth()->id() == $user_id) return 0;
+        return Status::select('statuses.song_id')
+        ->join('statuses as s1', function($join) {
+            $join->where('s1.user_id', auth()->id())
+            ->where('s1.state', 3)
+            ->on('statuses.song_id', '=', 's1.song_id');
+        })
+        ->where('statuses.user_id', $user_id)
+        ->where('statuses.state', 3)
+        ->count();
     }
 }
