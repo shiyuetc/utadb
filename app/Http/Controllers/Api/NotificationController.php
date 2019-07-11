57caea0d7dbf33868e8e5564f9ada5726b81dd57
shiyuetc <shiyuetc@users.noreply.github.com>
2019-07-11 17:03:01 +0900

    Fix notification controller

--- app/Http/Controllers/Api/NotificationController.php
+++ app/Http/Controllers/Api/NotificationController.php
@@ -9,9 +9,15 @@ use DB;
 
 class NotificationController extends ApiController
 {   
+    /**
+     * Get notification array addressed to logined user.
+     * 
+     * @param Request $request
+     * @return array $response
+     */
     public function index(Request $request)
     {
-        $notifications = Notification::select('notifications.id', 'notifications.sender_id', 'notifications.kind', 'notifications.created_at', DB::raw('posts.id as post_id'), 'posts.song_id')
+        $response = Notification::select('notifications.id', 'notifications.sender_id', 'notifications.kind', 'notifications.created_at', DB::raw('posts.id as post_id'), 'posts.song_id')
             ->leftjoin('posts', function($join) {
                 $join->where('notifications.kind', '=', 'like')
                     ->on('posts.id', '=', 'notifications.context_id');
@@ -21,11 +27,12 @@ class NotificationController extends ApiController
             ->orderBy('created_at', 'desc')
             ->get();
 
-        // 既読済みにする
+        // 全てを既読済みにする
         Notification::where('receiver_id', auth()->id())
             ->where('confirm', '=', 0)
             ->update(['confirm' => 1]);
 
-        return response()->json($notifications)->setStatusCode(200);
+        return response()->json($response)->setStatusCode(200);
     }
+
 }
