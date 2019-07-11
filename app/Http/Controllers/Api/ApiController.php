aa4fce2a95ca74bbdea4b374b0d312d25a78e624
shiyuetc <shiyuetc@users.noreply.github.com>
2019-07-11 16:18:51 +0900

    Add call exception function of api controller

--- app/Http/Controllers/Api/ApiController.php
+++ app/Http/Controllers/Api/ApiController.php
@@ -10,18 +10,32 @@ use Illuminate\Support\Facades\Auth;
 
 class ApiController extends Controller
 {   
-    public function QueryValidate($request, $validate = null)
+    /**
+     * The function is exception proccessing exclusively for api request.
+     * 
+     * @param string $message
+     * @param int $statusCode
+     * @return void
+     */
+    public function CallException($message, $statusCode = 500)
+    {
+        $response['errors'] = $message;
+        throw new HttpResponseException(response()->json($response, $statusCode));
+    }
+
+    /**
+     * The function is query validation exclusively for api request.
+     * 
+     * @param Request $request
+     * @param array $validate
+     * @return void
+     */
+    public function QueryValidate($request, $validate)
     {
         $validator = Validator::make($request->all(), $validate);
         if($validator->fails()) {
-            $response['errors'] = current(array_slice($validator->errors()->toArray(), 0, 1, true))[0];
-            throw new HttpResponseException(response()->json($response, 422));
+            $this->CallException(current(array_slice($validator->errors()->toArray(), 0, 1, true))[0], 422);
         }
     }
 
-    public function CallException($text, $statusCode)
-    {
-        $response['errors'] = $text;
-        throw new HttpResponseException(response()->json($response, $statusCode));
-    }
 }
