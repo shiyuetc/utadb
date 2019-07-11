cbd040bae32e8d18bfcc5dccf93a589df7fdba3c
shiyuetc <shiyuetc@users.noreply.github.com>
2019-07-11 12:27:30 +0900

    Fix songs api

--- app/Http/Controllers/Api/SongController.php
+++ app/Http/Controllers/Api/SongController.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Http\Requests\ApiRequestRules;
 use App\Libraries\SongPuller\Puller;
 use App\Models\Song;
 use App\Models\Status;
@@ -11,159 +10,162 @@ use DB;
 
 class SongController extends ApiController
 {   
-    public function search(Request $request)
+    /**
+     * Get song from local and global database.
+     * 
+     * @param Request $request
+     * @return array $response
+     */
+    public function index(Request $request)
     {
+        // Use validation.
         $this->QueryValidate($request, [
-            'source' => ApiRequestRules::getSourceRule(),
-            'q' => ApiRequestRules::getQRule(),
-            'page' => ApiRequestRules::getPageRule(),
+            'type' => 'sometimes|string|in:title,artist',
+            'source' => 'nullable|numeric|between:-1,1',
+            'keyword' => 'required|string|between:1,20',
+            'page' => 'nullable|numeric|between:1,9999',
+            'per_page' => 'nullable|numeric|between:1,20',
         ]);
-        
+
+        // Declare variable.
         $response = [];
-        if($request->source == -1) {
-            $songs = Song::where('title', 'like', "%{$request->q}%")
-                ->orWhere('artist', 'like', "%{$request->q}%")
-                ->orderBy('title')
-                ->skip(($request->query('page', 1) - 1) * 10)
-                ->take(10)
-                ->get();
-        } else {
-            $songs = Puller::searchSong($request->source, $request->q, $request->query('page', 1));
-        }
+        $type = $request->query('type', 'title');
+        $source = $request->query('source', -1);
+        $keyword = $request->keyword;
+        $page = $request->query('page', 1);
+        $per_page = $request->query('per_page', 20);
 
-        if(count($songs) > 0) {
-            $song_ids = array();
-            foreach($songs as $song) {
-                $song_ids[] = $song["id"];
+        if($type == 'title') { // Searching from song title or artist name.
+            if($source == -1) { // From local database.
+                $response = Song::where('title', 'like', "%{$keyword}%")
+                    ->orWhere('artist', 'like', "%{$keyword}%")
+                    ->orderBy('title')
+                    ->skip(($page - 1) * $per_page)
+                    ->take($per_page)
+                    ->get();
+            } else { // From global database.
+                $response = Puller::searchSong($source, $keyword, $page);
             }
-            
-            $states = Status::select('song_id', DB::raw('state as my_state'))
-                ->where('user_id', auth()->id())
-                ->whereIn('song_id', $song_ids)
-                ->get();
-
-            $temp_my_state = [];
-            foreach($states as $state)
-            {
-                $temp_my_state[$state->song_id] = $state->my_state;
-            }
-            for($i = 0; $i < count($songs); $i++)
-            {
-                $response[] = [
-                    'my_state' => isset($temp_my_state[$songs[$i]['id']]) ? $temp_my_state[$songs[$i]['id']] : 0,
-                    'song' => $songs[$i]
-                ];
+        } elseif($type == 'artist') { // Searching from artist id.
+            if($source == -1) { // From local database.
+                $response = Song::Where('artist_id', $keyword)
+                    ->orderBy('title')
+                    ->skip(($page - 1) * $per_page)
+                    ->take($per_page)
+                    ->get();
+            } else { // From gloval database.
+                $response = Puller::searchSongFromArtist($keyword, $page);
             }
         }
 
         return response()->json($response)->setStatusCode(200);
     }
-    
-    public function searchFromArtist(Request $request)
+
+    /**
+     * Get song from registered song list.
+     * 
+     * @param Request $request
+     * @return array $response
+     */
+    public function user(Request $request, $id)
     {
+        // Use validation.
         $this->QueryValidate($request, [
-            'page' => ApiRequestRules::getPageRule(),
+            'state' => 'nullable|numeric|between:0,3',
+            'keyword' => 'sometimes|string|between:1,20',
+            'page' => 'nullable|numeric|between:1,9999',
+            'per_page' => 'nullable|numeric|between:1,50',
         ]);
-        
+
+        // Checking if user exist.
+        if(!DB::table('users')->where('id', $id)->exists()) {
+            $this->CallException(__('passwords.user'), 404);
+        }
+
+        // Declare variable.
         $response = [];
-        if($request->source == -1) {
-            $songs = Song::Where('artist_id', $request->artist_id)
-                ->orderBy('title')
-                ->skip(($request->query('page', 1) - 1) * 10)
-                ->take(10)
-                ->get();
-        } else {
-            $songs = Puller::searchSongFromArtist($request->artist_id, $request->query('page', 1));
-        }
-        
-        if(count($songs) > 0) {
-            $song_ids = array();
-            foreach($songs as $song) {
-                $song_ids[] = $song["id"];
-            }
-            
-            $states = Status::select('song_id', DB::raw('state as my_state'))
-                ->where('user_id', auth()->id())
-                ->whereIn('song_id', $song_ids)
-                ->get();
+        $state = $request->query('state', 0);
+        $keyword = $request->query('keyword', null);
+        $page = $request->query('page', 1);
+        $per_page = $request->query('per_page', 50);
 
-            $temp_my_state = [];
-            foreach($states as $state)
-            {
-                $temp_my_state[$state->song_id] = $state->my_state;
-            }
-            for($i = 0; $i < count($songs); $i++)
-            {
-                $response[] = [
-                    'my_state' => isset($temp_my_state[$songs[$i]['id']]) ? $temp_my_state[$songs[$i]['id']] : 0,
-                    'song' => $songs[$i]
-                ];
-            }
-        }
+        $query = Status::select('statuses.song_id')
+            ->where('statuses.user_id', $id);
 
-        return response()->json($response)->setStatusCode(200);
-    }
-    
-    public function userCommon(Request $request)
-    {
-        $this->QueryValidate($request, [
-            'id' => ApiRequestRules::getUserIdRule(),
-            'page' => ApiRequestRules::getPageRule(),
-        ]);
+        // Filter by select state.
+        if($state != 0) $query = $query->where('statuses.state', $state);
 
-        $response = Status::select('statuses.song_id', DB::raw('s1.state as my_state'))
-        ->join('statuses as s1', function($join) {
-            $join->where('s1.user_id', auth()->id())
-            ->where('s1.state', 3)
-            ->on('statuses.song_id', '=', 's1.song_id');
-        })
-        ->where('statuses.user_id', $request->id)
-        ->where('statuses.state', 3)
-        ->with('song')
-        ->join('songs', 'statuses.song_id', '=', 'songs.id')
-        ->orderBy('artist')
-        ->skip(($request->query('page', 1) - 1) * 50)
-        ->take(50)
-        ->get();
-
-        return response()->json($response)->setStatusCode(200);
-    }
-
-    public function userStatuses(Request $request)
-    {
-        $this->QueryValidate($request, [
-            'id' => ApiRequestRules::getUserIdRule(),
-            'state' => ApiRequestRules::getStateRule(),
-            'page' => ApiRequestRules::getPageRule(),
-        ]);
-
-        $query = Status::select('statuses.song_id', DB::raw('IFNULL(s1.state, 0) as my_state'))
-        ->leftjoin('statuses as s1', function($join) {
-            $join->where('s1.user_id', auth()->id())
-            ->on('statuses.song_id', '=', 's1.song_id');
-        })
-        ->where('statuses.user_id', $request->id);
-
-        // 対象の状態を取得
-        if($request->state != 0) $query = $query->where('statuses.state', $request->state);
-
-        // キーワードからの絞り込み
-        if(!is_null($request->q)) {
-            $q = $request->q;
-            $query = $query->where(function($where) use (&$q) {
-                $where->where('songs.title', 'like', "%{$q}%")
-                ->orWhere('songs.artist', 'like', "%{$q}%");
+        // Filter by keyword.
+        if(!is_null($keyword)) {
+            $query = $query->where(function($where) use (&$keyword) {
+                $where->where('songs.title', 'like', "%{$keyword}%")
+                ->orWhere('songs.artist', 'like', "%{$keyword}%");
             });
         }
 
-        $response = $query
+        // Send query.
+        $temp_response = $query
             ->with('song')
-            ->join('songs', 'statuses.song_id', '=', 'songs.id')
+            ->join('songs', 'statuses.song_id', 'songs.id')
             ->orderBy('artist')
-            ->skip(($request->query('page', 1) - 1) * 50)
-            ->take(50)
+            ->skip(($page - 1) * $per_page)
+            ->take($per_page)
             ->get();
+
+        // Take out the contents.
+        foreach($temp_response as $temp) {
+            $response[] = $temp->song;
+        }
             
         return response()->json($response)->setStatusCode(200);
     }
+
+    /**
+     * Get song from common song list.
+     * 
+     * @param Request $request
+     * @return array $response
+     */
+    public function common(Request $request, $id)
+    {
+        // Use validation.
+        $this->QueryValidate($request, [
+            'page' => 'nullable|numeric|between:1,9999',
+            'per_page' => 'nullable|numeric|between:1,50',
+        ]);
+
+        // Checking if user exist.
+        if(!DB::table('users')->where('id', $id)->exists()) {
+            $this->CallException(__('passwords.user'), 404);
+        }
+
+        // Declare variable.
+        $response = [];
+        $page = $request->query('page', 1);
+        $per_page = $request->query('per_page', 50);
+
+        // Send query.
+        $temp_response = Status::select('statuses.song_id')
+            ->join('statuses as s1', function($join) {
+                $join->where('s1.user_id', auth()->id())
+                ->where('s1.state', 3)
+                ->on('statuses.song_id', 's1.song_id');
+            })
+            ->where('statuses.user_id', $id)
+            ->where('statuses.state', 3)
+            ->with('song')
+            ->join('songs', 'statuses.song_id', 'songs.id')
+            ->orderBy('artist')
+            ->skip(($page - 1) * $per_page)
+            ->take($per_page)
+            ->get();
+
+        // Take out the contents.
+        foreach($temp_response as $temp) {
+            $response[] = $temp->song;
+        }
+
+        return response()->json($response)->setStatusCode(200);
+    }
+    
 }
