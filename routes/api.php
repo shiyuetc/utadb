260d43340270d7bc096342272b6231dab57034f2
shiyuetc <shiyuetc@users.noreply.github.com>
2019-07-11 12:28:01 +0900

    Fix api routes

--- routes/api.php
+++ routes/api.php
@@ -18,12 +18,15 @@ Route::group(['middleware' => 'api'], function(){
     
     Route::get('user_timeline', 'Api\PostController@userTimeline');
     Route::get('public_timeline', 'Api\PostController@publicTimeline');
-
-    Route::get('user_statuses', 'Api\SongController@userStatuses');
-
+    
     Route::get('statuses/lookup', 'Api\StatusController@lookup');
+    Route::get('statuses/registereUser', 'Api\StatusController@registereUser');
 
-    Route::get('songs/search_from_artist', 'Api\SongController@searchFromArtist');
+    Route::prefix('songs')->group(function() {
+        Route::get('/', 'Api\SongController@index');
+        Route::get('{id}', 'Api\SongController@user');
+        Route::get('{id}/common', 'Api\SongController@common');
+    });
 });
 
 Route::group(['middleware' => ['api', 'auth.api']], function(){
@@ -31,15 +34,12 @@ Route::group(['middleware' => ['api', 'auth.api']], function(){
 
     Route::get('avatars/search', 'Api\AvatarController@search');
 
-    Route::get('songs/search', 'Api\SongController@search');
-    Route::get('user_common', 'Api\SongController@userCommon');
-
     Route::prefix('users')->group(function() {
         Route::get('list', 'Api\UserController@list');
         Route::get('search', 'Api\UserController@search');
     });
     
-    Route::post('statuses/update', 'Api\StatusController@edit');
+    Route::post('statuses/update', 'Api\StatusController@update');
 
     Route::post('likes/create', 'Api\LikeController@create');
     Route::post('likes/destroy', 'Api\LikeController@destroy');
