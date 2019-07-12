c29def95981df31230013244c4c2aae940aaeb32
shiyuetc <shiyuetc@users.noreply.github.com>
2019-07-12 19:49:21 +0900

    Fix

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -8,18 +8,20 @@ class SearchController extends Controller
 {
     public function searchUser(Request $request)
     {
-        $q = isset($request->q) ? urlencode(trim($request->q)) : '';
-        $page = (isset($request->page) && $request->page >= 1 && $request->page <= 9999) ? $request->page : 1;
-
-        return view('pages.search.user', ['q' => $q, 'page' => $page]);
+        $response = [
+            'q' => isset($request->q) ? urlencode(trim($request->q)) : '',
+            'page' => (isset($request->page) && $request->page >= 1 && $request->page <= 9999) ? $request->page : 1
+        ];
+        return view('pages.search.user', $response);
     }
 
     public function searchSong(Request $request)
     {
-        $source = (isset($request->source) && $request->source >= 0 && $request->source <= 1) ? $request->source : 0;
-        $q = isset($request->q) ? urlencode(trim($request->q)) : '';
-        $page = (isset($request->page) && $request->page >= 1 && $request->page <= 9999) ? $request->page : 1;
-
-        return view('pages.search.song', ['source' => $source, 'q' => $q, 'page' => $page]);
+        $response = [
+            'source' => (isset($request->source) && $request->source >= 0 && $request->source <= 1) ? $request->source : 0,
+            'q' => isset($request->q) ? urlencode(trim($request->q)) : '',
+            'page' => (isset($request->page) && $request->page >= 1 && $request->page <= 9999) ? $request->page : 1
+        ];
+        return view('pages.search.song', $response);
     }
 }
