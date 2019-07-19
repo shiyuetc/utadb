bc930a7dae7283b99f477d917d867002463a777e
shiyuetc <shiyuetc@users.noreply.github.com>
2019-07-19 17:44:43 +0900

    Fix user page

--- resources/views/pages/user.blade.php
+++ resources/views/pages/user.blade.php
@@ -8,10 +8,20 @@
 @section('content')
 @component('components.section')
 @slot('title')
+  <i class="fas fa-chart-bar"></i>&nbsp;分析データ
+@endslot
+@slot('contents')
+<artist-rate-pie-component :user_id="'{{ $user->id }}'" :registered_count="{{ $user->stacked_count + $user->training_count + $user->mastered_count }}"></artist-rate-pie-component>
+<status-stacked-component :user_id="'{{ $user->id }}'"></status-stacked-component>
+@endslot
+@endcomponent
+
+@component('components.section')
+@slot('title')
   <i class="fab fa-react"></i>&nbsp;ユーザータイムライン
 @endslot
 @slot('contents')
-  <timeline-component :user_id="'{{ $user->id }}'"/>
+  <timeline-component :user_id="'{{ $user->id }}'" :screen_name="'{{ $user->screen_name }}'" :count="5"/>
 @endslot
 @endcomponent
 @endsection
