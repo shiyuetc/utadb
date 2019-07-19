cd713e955a39940b04bd9c9431d3324115a71faa
shiyuetc <shiyuetc@users.noreply.github.com>
2019-07-19 17:45:18 +0900

    Add components

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -31,6 +31,9 @@ Vue.component('song-infomation-component', require('./components/song-infomation
 
 Vue.component('user-search-component', require('./components/user-search.vue'));
 
+Vue.component('artist-rate-pie-component', require('./components/artist-rate-pie.vue'));
+Vue.component('status-stacked-component', require('./components/status-stacked.vue'));
+
 const app = new Vue({
     el: '#app',
     methods: {
