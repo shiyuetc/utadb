b0e0c22727982b694ca59ad86bb2abc8ab1771ab
shiyuetc <shiyuetc@users.noreply.github.com>
2019-07-19 17:27:25 +0900

    Add user records page

---
+++ resources/views/pages/user-records.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.app')
+@section('title', "{$user->name}(@{$user->screen_name})さんの投稿")
+
+@section('sidebar')
+@include('widgets.user-infomation', ['user' => $user])
+@endsection
+
+@section('content')
+@component('components.section')
+@slot('title')
+  <i class="fab fa-react"></i>&nbsp;ユーザータイムライン
+@endslot
+@slot('contents')
+  <timeline-component :user_id="'{{ $user->id }}'"/>
+@endslot
+@endcomponent
+@endsection
