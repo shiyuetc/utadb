1dc96f415a54ae6529ec260f1cc767f729afb355
shiyuetc <shiyuetc@users.noreply.github.com>
2019-07-19 17:30:08 +0900

    Add artist rate api

--- app/Http/Controllers/Api/ApplicationController.php
+++ app/Http/Controllers/Api/ApplicationController.php
@@ -3,8 +3,10 @@
 namespace App\Http\Controllers\Api;
 
 use App\Models\Post;
+use App\Models\Status;
 use App\Models\User;
 use Illuminate\Http\Request;
+use DB;
 
 class ApplicationController extends ApiController
 {   
@@ -21,4 +23,16 @@ class ApplicationController extends ApiController
         ])->setStatusCode(200);
     }
 
+    public function artistRate(Request $request)
+    {
+        $response = Status::select(['artist', DB::raw('count(*) as count')])
+            ->where('statuses.user_id', $request->id)
+            ->join('songs', 'statuses.song_id', 'songs.id')
+            ->orderBy('count', 'desc')
+            ->groupBy('artist')
+            ->limit(10)
+            ->get();
+
+        return response()->json($response)->setStatusCode(200);
+    }
 }
