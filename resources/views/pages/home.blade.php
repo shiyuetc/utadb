c8d284a606dae90914354d6979e9d25d21f92eb7
shiyuetc <shiyuetc@users.noreply.github.com>
2019-08-02 17:21:19 +0900

    Add outer link section

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,7 +1,18 @@
 @extends('layouts.app')
 
 @section('sidebar')
-@include('widgets.user-infomation', ['user' => Auth::user(), 'hidden' => true])
+  @include('widgets.user-infomation', ['user' => Auth::user(), 'hidden' => true])
+  @component('components.section', ['hidden' => true, 'toggle' => true])
+  @slot('title')
+    <i class="fas fa-link"></i>&nbsp;外部関連リンク
+  @endslot
+  @slot('contents')
+    <ul>
+      <li><a class="default-link" href="https://www.joysound.com/web/" target="_blank">JOYSOUND.com</a></li>
+      <li><a class="default-link" href="https://www.clubdam.com/" target="_blank">DAM CHANNEL</a></li>
+    </ul>
+  @endslot
+  @endcomponent
   @include('widgets.tweet-notification')
 @endsection
 
