deb74422f262bfcacdb15ed14eeaff490195404e
shiyuetc <shiyuetc@users.noreply.github.com>
2019-08-04 13:05:19 +0900

    Add relation

--- app/Models/Like.php
+++ app/Models/Like.php
@@ -9,5 +9,14 @@ class Like extends Model
     protected $table = 'likes';
 
     public $timestamps = false;
+    
+    public function post()
+    {
+        return $this->belongsTo('App\Models\Post');
+    }
 
+    public function user()
+    {
+        return $this->belongsTo('App\Models\User');
+    }
 }
