22ea77cb747e83e9a9ddd50275bb91391166ad30
shiyuetc <shiyuetc@users.noreply.github.com>
2019-12-01 11:38:04 +0900

    Add list angle style

--- resources/views/widgets/setting-list.blade.php
+++ resources/views/widgets/setting-list.blade.php
@@ -3,7 +3,7 @@
   <i class="fas fa-cog"></i>&nbsp;設定
 @endslot
 @slot('contents')
-  <ul class="setting-list list">
+  <ul class="list list-angle">
     <li class="{{ Request::is('settings/profile') ? ' active' : '' }}"><a href="{{ route('settings.profile') }}"><i class="far fa-address-card"></i>&nbsp;プロフィールの設定<span class="right-icon"><i class="fas fa-angle-double-right"></i></span></a></li>
     <li class="{{ Request::is('settings/account') ? ' active' : '' }}"><a href="{{ route('settings.account') }}"><i class="fas fa-user"></i>&nbsp;アカウントの設定<span class="right-icon"><i class="fas fa-angle-double-right"></i></span></a></li>
     <li class="{{ Request::is('settings/other') ? ' active' : '' }}"><a href="{{ route('settings.other') }}"><i class="fas fa-cogs"></i>&nbsp;その他<span class="right-icon"><i class="fas fa-angle-double-right"></i></span></a></li>
