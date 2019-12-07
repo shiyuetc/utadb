6c106d1d3baf0cb2fb0a76ca9f67d83ce55c3bb4
shiyuetc <shiyuetc@users.noreply.github.com>
2019-12-07 09:50:30 +0900

    Fix localhost port

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -12,9 +12,9 @@ class UsersTableSeeder extends Seeder
     public function run()
     {
         DB::table('users')->insert([
-            [ 'screen_name' => 'test1', 'name' => 'test_user1', 'profile_image_url' => 'http://localhost/images/profile_image/monster/monster_a', 'password' => bcrypt('password'), 'remember_token' => str_random(10) ],
-            [ 'screen_name' => 'test2', 'name' => 'test_user2', 'profile_image_url' => 'http://localhost/images/profile_image/monster/monster_b', 'password' => bcrypt('password'), 'remember_token' => str_random(10) ],
-            [ 'screen_name' => 'test3', 'name' => 'test_user3', 'profile_image_url' => 'http://localhost/images/profile_image/monster/monster_c', 'password' => bcrypt('password'), 'remember_token' => str_random(10) ],
+            [ 'screen_name' => 'test1', 'name' => 'test_user1', 'profile_image_url' => 'http://localhost:8000/images/profile_image/monster/monster_a', 'password' => bcrypt('password'), 'remember_token' => str_random(10) ],
+            [ 'screen_name' => 'test2', 'name' => 'test_user2', 'profile_image_url' => 'http://localhost:8000/images/profile_image/monster/monster_b', 'password' => bcrypt('password'), 'remember_token' => str_random(10) ],
+            [ 'screen_name' => 'test3', 'name' => 'test_user3', 'profile_image_url' => 'http://localhost:8000/images/profile_image/monster/monster_c', 'password' => bcrypt('password'), 'remember_token' => str_random(10) ],
         ]);
 
         factory(App\Models\User::class, 10)->create();
