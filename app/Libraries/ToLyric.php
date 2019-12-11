c84059afdc57e438578a5c140e85b5f7d1bc7526
shiyuetc <shiyuetc@users.noreply.github.com>
2019-12-11 12:32:15 +0900

    Fix bug

--- app/Libraries/ToLyric.php
+++ app/Libraries/ToLyric.php
@@ -4,30 +4,47 @@ namespace App\Libraries;
 
 class ToLyric
 {
-    public static function get($artist, $title)
+    public static function get($input_artist, $input_title)
     {
-        $artist = str_replace(' ', '+', $artist);
-        $title = str_replace(' ', '+', $title);
+        $artist = str_replace(' ', '+', $input_artist);
+        $title = str_replace(' ', '+', $input_title);
         $response = [];
         try {
             $html = file_get_contents("http://search.j-lyric.net/?ct=2&ca=2&cl=2&ka={$artist}&kt={$title}");
             $doc = \phpQuery::newDocument($html)->find("#mnb");
-            $title = pq($doc["div.bdy"])->find("p.mid a")->text();
-            $artist = pq($doc["div.bdy"])->find("p.sml a")->text();
-            $url = pq($doc["div.bdy"])->find("p.mid a")->attr("href");
-            if ($title != "" && $artist != "") {
-                $html = file_get_contents($url);
+            $songs = [];
+            foreach($doc["div.bdy"] as $bdy) {
+                $artist = pq($bdy)->find("p.sml a")->text();
+                $title = pq($bdy)->find("p.mid a")->text();
+                $url = pq($bdy)->find("p.mid a")->attr("href");
+                if ($title != "" && $artist != "") {
+                    $songs[] = [
+                        "title" => $title,
+                        "artist" => $artist,
+                        "url" => $url
+                    ];
+                } else {
+                    break;
+                }
+            }
+            if(count($songs) > 0) {
+                foreach($songs as $song) {
+                    if($song["artist"] == $input_artist && $song["title"] == $input_title) {
+                        $response = $song;
+                        break;
+                    }
+                }
+                if(count($response) == 0) {
+                    $response = $songs[0];
+                }
+                $html = file_get_contents($response["url"]);
                 $doc = \phpQuery::newDocument($html)->find("#mnb");
                 $lyric = pq($doc)->find("p#Lyric")->html();
-                $response = array(
-                    "title" => $title,
-                    "artist" => $artist,
-                    "lyric" => $lyric
-                );
+                $response["lyric"] = $lyric;
             } else {
                 $response["errors"] = array("message" => "Not Found.");
             }
-        } catch (Exception $e) {
+        } catch (\Exception $e) {
             $response["errors"] = array("message" => $e->getMessage());
         }
         return $response;
