032c3ecc31e1b2c3b9be9fa71bf1e78d963d9725
shiyuetc <shiyuetc@users.noreply.github.com>
2019-12-14 22:44:18 +0900

    Fix authenticate logic

--- resources/assets/js/router.js
+++ resources/assets/js/router.js
@@ -3,17 +3,35 @@ import VueRouter from 'vue-router';
 
 Vue.use(VueRouter);
 
-const routes = [
-  { 
-    path: '/', 
-    name: 'home', 
-    component: require('./components/Home.vue')
-  }
-];
+const routes = [{
+  path: '/',
+  name: 'home',
+  component: require('./components/Home.vue')
+}];
 
 const router = new VueRouter({
   mode: 'history',
   routes,
 })
+
+router.beforeEach((to, from, next) => {
+  store.commit('alert/setAlert', {
+    'message': ''
+  });
+  if (to.matched.some(record => record.meta.requiresAuth)) {
+    if (!store.getters['auth/isLogin']) {
+      next({
+        path: '/login',
+        query: {
+          redirect: to.fullPath
+        }
+      });
+    } else {
+      next();
+    }
+  } else {
+    next();
+  }
+});
 
 export default router;
