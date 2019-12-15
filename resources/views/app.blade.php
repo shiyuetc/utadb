c5f73045dcb12f59871d0313ceecd20e8a62f2be
shiyuetc <shiyuetc@users.noreply.github.com>
2019-12-15 09:22:54 +0900

    Use scss

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,7 +6,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta name="csrf-token" content="{{ csrf_token() }}">
   <title>{{ config('app.name', 'Utadb') }}</title>
-  <link rel="stylesheet" href="/css/style.css">
+  <link rel="stylesheet" href="/css/app.css">
   <link rel="stylesheet" href="/css/template.css">
   <link rel="stylesheet" type="text/css" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
   <script>
