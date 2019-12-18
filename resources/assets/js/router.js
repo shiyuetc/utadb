eceb2b19081b48aa55ace2396c589db38c3352ef
shiyuetc <shiyuetc@users.noreply.github.com>
2019-12-18 10:52:36 +0900

    Implement branch route

--- resources/assets/js/router.js
+++ resources/assets/js/router.js
@@ -7,18 +7,27 @@ Vue.use(VueRouter);
 const routes = [
   {
     path: '/',
+    name: 'welcome',
+    component: require('./components/pages/Welcome.vue'),
+    meta: { requiresGuest: true }
+  },
+  {
+    path: '/',
     name: 'home',
     component: require('./components/pages/Home.vue'),
+    meta: { requiresAuth: true }
   },
   {
     path: '/register',
     name: 'register',
     component: require('./components/pages/Register.vue'),
+    meta: { requiresGuest: true }
   },
   {
     path: '/login',
     name: 'login',
     component: require('./components/pages/Login.vue'),
+    meta: { requiresGuest: true }
   },
 ];
 
@@ -29,18 +38,19 @@ const router = new VueRouter({
 
 router.beforeEach((to, from, next) => {
   store.commit('alert/setAlert', { 'message': '' });
+  var next_flag = true;
   if (to.matched.some(record => record.meta.requiresAuth)) {
     if (!store.getters.isLogin) {
-      next({
-        path: '/login',
-        query: {
-          redirect: to.fullPath
-        }
-      });
-    } else {
-      next();
+      next_flag = false;
+      next({ name: 'login' });
     }
-  } else {
+  } else if (to.matched.some(record => record.meta.requiresGuest)) {
+    if (store.getters.isLogin) {
+      next_flag = false;
+      next({ name: 'home' });
+    }
+  }
+  if(next_flag) {
     next();
   }
 });
