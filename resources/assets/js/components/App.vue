1ffa24b0395187f5250c8de56fd38b78e879803b
shiyuetc <shiyuetc@users.noreply.github.com>
2019-12-19 08:39:19 +0900

    Fix app

--- resources/assets/js/components/App.vue
+++ resources/assets/js/components/App.vue
@@ -1,31 +1,37 @@
 <template>
-  <div id="app">
+<div id="app">
+  <div id="app-body">
+    <vue-progress-bar />
+    <v-header />
+    <div class="header-push"></div>
     <div class="container">
       <router-view></router-view>
     </div>
-    <vue-progress-bar></vue-progress-bar>
+    <div class="footer-push"></div>
   </div>
+  <v-footer />
+</div>
 </template>
 
 <script>
 export default {
-  name: 'App',
-  mounted () {
-    this.$Progress.finish()
+  name: "App",
+  mounted() {
+    this.$Progress.finish();
   },
-  created () {
-    this.$Progress.start()
+  created() {
+    this.$Progress.start();
     this.$router.beforeEach((to, from, next) => {
       if (to.meta.progress !== undefined) {
-        let meta = to.meta.progress
-        this.$Progress.parseMeta(meta)
+        let meta = to.meta.progress;
+        this.$Progress.parseMeta(meta);
       }
-      this.$Progress.start()
-      next()
-    })
+      this.$Progress.start();
+      next();
+    });
     this.$router.afterEach((to, from) => {
-      this.$Progress.finish()
-    })
+      this.$Progress.finish();
+    });
   }
-}
+};
 </script>
